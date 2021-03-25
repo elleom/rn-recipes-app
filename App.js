c0510898ad1e1c3e0750842a2a06708699962dbc
0elle0 <el_leo_m@hotmail.com>
2021-03-26 00:27:36 +0100

    enableScreens()

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
-import {StatusBar} from 'expo-status-bar';
 import React, {useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import AppLoading from "expo-app-loading";
 import * as Font from 'expo-font'; // npm install --save expo-font
 import MealsNavigator from "./navigation/MealsNavigator";
+import {enableScreens} from 'react-native-screens';
+
+enableScreens();
 
 const fetchFonts = () => {
     Font.loadAsync(
