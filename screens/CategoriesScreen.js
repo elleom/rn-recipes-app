0e8aa3039c5948f0f87a5ff2711e3f49343dfa87
0elle0 <el_leo_m@hotmail.com>
2021-03-25 20:56:55 +0100

    constants + Oppacity

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,32 +1,51 @@
 import React from 'react';
 import {View, Text, StyleSheet, Button, FlatList} from "react-native";
 import CategoryMealsScreen from "./CategoryMealsScreen";
+import {Dimensions} from "react-native";
 
 import {CATEGORIES} from "../data/dummy-data";
 import {TouchableOpacity} from "react-native";
 
-const renderGridItem = itemData => {
-    return (
-        <TouchableOpacity>
-            <View>
-                <Text>{itemData.item.title}</Text>
-            </View>
-        </TouchableOpacity>
-    )
-}
-
 const CategoriesScreen = (props) => {
+
+    const renderGridItem = itemData => {
+        return (
+            <TouchableOpacity onPress={() => {props.navigation.navigate('CategoryMeals')}} >
+                <View style={styles.gridItem} >
+                    <Text>{itemData.item.title}</Text>
+                </View>
+            </TouchableOpacity>
+        )
+    }
+
+
     return ( // latest rn does not need a keyExtractor on a FlatList component :)
         <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2}/>
     )
 };
+
+CategoriesScreen.navigationOptions = {
+    headerTittle: 'Meal Categories',
+    headerStyle: {
+        backgroundColor: 'white'
+    }
+}
 
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    gridItem: {
+        flex: 1,
+        margin: 15,
+        height: Dimensions.get('window').height /7, //window works better on android
+
+
     }
+
+
 })
 
 export default CategoriesScreen;
