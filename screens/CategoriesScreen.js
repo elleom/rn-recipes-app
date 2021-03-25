e2a01226a011bd02326f31ea0b20407ac568ee11
0elle0 <el_leo_m@hotmail.com>
2021-03-25 21:25:07 +0100

    navigationOptions

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,34 +1,36 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button, FlatList} from "react-native";
+import {View, Text, StyleSheet, Button, FlatList, Platform} from "react-native";
 import CategoryMealsScreen from "./CategoryMealsScreen";
 import {Dimensions} from "react-native";
 
 import {CATEGORIES} from "../data/dummy-data";
 import {TouchableOpacity} from "react-native";
+import Colors from "../constants/Colors";
 
 const CategoriesScreen = (props) => {
 
     const renderGridItem = itemData => {
         return (
-            <TouchableOpacity onPress={() => {props.navigation.navigate('CategoryMeals')}} >
-                <View style={styles.gridItem} >
+            <TouchableOpacity onPress={() => {
+                props.navigation.navigate('CategoryMeals')
+            }}>
+                <View style={styles.gridItem}>
                     <Text>{itemData.item.title}</Text>
                 </View>
             </TouchableOpacity>
         )
     }
-
-
     return ( // latest rn does not need a keyExtractor on a FlatList component :)
         <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2}/>
     )
 };
 
 CategoriesScreen.navigationOptions = {
-    headerTittle: 'Meal Categories',
+    headerTitle: 'Meal Categories',
     headerStyle: {
-        backgroundColor: 'white'
-    }
+        backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : '' //if none described then default
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
 }
 
 const styles = StyleSheet.create({
@@ -40,7 +42,7 @@ const styles = StyleSheet.create({
     gridItem: {
         flex: 1,
         margin: 15,
-        height: Dimensions.get('window').height /7, //window works better on android
+        height: Dimensions.get('window').height / 7, //window works better on android
 
 
     }
