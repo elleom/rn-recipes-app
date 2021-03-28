a15b3ff9a6b3bc2083ce541a9f76f9c23922f28d
0elle0 <el_leo_m@hotmail.com>
2021-03-29 00:27:14 +0200

    add grid style

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -5,7 +5,7 @@ import {getBackgroundColor} from "react-native/Libraries/LogBox/UI/LogBoxStyle";
 const CategoryGridTile = props => {
     return (
         <TouchableOpacity style={styles.gridItem} onPress={props.onSelect}>
-            <View style={{backgroundColor: props.color}}>
+            <View style={{...styles.container, ...{backgroundColor: props.color}}}>
                 <Text>{props.title}</Text>
             </View>
         </TouchableOpacity>
@@ -16,7 +16,21 @@ const styles = StyleSheet.create({
     gridItem: {
         flex: 1,
         margin: 15,
-        height: Dimensions.get('window').height / 7, //window works better on androidw
+        height: 150, //window works better on androidw
+    },
+    container: {
+        flex: 1,
+        borderRadius: 10,
+        shadowColor: 'black',
+        shadowOpacity: 0.26,
+        shadowOffset: {width: 0, height: 2},
+        shadowRadius: 10,
+        elevation: 3,
+        padding: 15,
+        justifyContent: 'flex-end',
+        alignItems: 'flex-end'
+
+
     }
 })
 
