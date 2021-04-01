dc30b1bf642bb2278469b061c3bcb0aa2f51c4eb
0elle0 <el_leo_m@hotmail.com>
2021-04-02 00:37:07 +0200

    add favourite icon

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 
 import { MEALS } from '../data/dummy-data';
+import HeaderButton from "../components/HeaderButton";
+import {HeaderButtons, Item} from "react-navigation-header-buttons";
 
 const MealDetailScreen = props => {
     const mealId = props.navigation.getParam('mealId');
@@ -25,7 +27,11 @@ MealDetailScreen.navigationOptions = navigationData => {
     const mealId = navigationData.navigation.getParam('mealId');
     const selectedMeal = MEALS.find(meal => meal.id === mealId);
     return {
-        headerTitle: selectedMeal.title
+        headerTitle: selectedMeal.title,
+        headerRight: () =>
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item title='Favourite' iconName='ios-star' onPress={() => {console.log('marked as favourite')}} />
+            </HeaderButtons>
     };
 };
 
