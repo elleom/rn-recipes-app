bba29f8da156bf8757415cc12ba0f607607485de
0elle0 <el_leo_m@hotmail.com>
2021-04-05 21:21:37 +0200

    switches

--- screens/FilterScreen.js
+++ screens/FilterScreen.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import {View, Text, StyleSheet} from "react-native";
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Switch} from "react-native";
 import {HeaderButtons, Item} from "react-navigation-header-buttons";
 import HeaderButton from "../components/HeaderButton";
+import Colors from "../constants/Colors";
 
-const FilterScreen = () => {
+const FilterSwitch = props => {
+    return (
+        <View style={styles.filterContainer}>
+            <Text>{props.title}</Text>
+            <Switch
+                trackColor={{true: Colors.primaryColor}}
+                value={props.state} onValueChange={props.onChange()}/>
+        </View>
+    )
+
+
+}
+
+const FilterScreen = props => {
+    const [isGlutenFree, setGlutenFree] = useState(false);
+    const [isVegetarian, setVegetarian] = useState(false);
+    const [isVegan, setVegan] = useState(false);
+    const [isLactoseFree, setLactoseFree] = useState(false);
+
     return (
         <View style={styles.screen}>
-            <Text>This is the Filter Screen</Text>
+            <Text style={styles.title}>Available Filters / Restrictions</Text>
+            <FilterSwitch title={'Gluten-Free'} state={isGlutenFree} onChange={newValue => setGlutenFree(newValue)} />
+            <FilterSwitch title={'Vegetarian'} state={isVegetarian} onChange={newValue => setVegetarian(newValue)} />
+            <FilterSwitch title={'Vegan'} state={isVegan} onChange={newValue => setVegan(newValue)} />
+            <FilterSwitch title={'Lactose-Free'} state={isLactoseFree} onChange={newValue => setLactoseFree(newValue)} />
         </View>
     )
 };
@@ -30,8 +53,21 @@ FilterScreen.navigationOptions = navData => {
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center'
+    },
+    title: {
+        fontFamily: 'open-sans',
+        fontSize: 20,
+        margin: 20,
+        textAlign: 'center'
+    },
+    filterContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: "80%"
+
+
     }
 })
 
