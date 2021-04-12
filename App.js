329a2ba1d31a195335b0e50b1832dde2c7783b35
0elle0 <el_leo_m@hotmail.com>
2021-04-12 23:00:17 +0200

    provider wrap

--- App.js
+++ App.js
@@ -4,8 +4,16 @@ import AppLoading from "expo-app-loading";
 import * as Font from 'expo-font'; // npm install --save expo-font
 import MealsNavigator from "./navigation/MealsNavigator";
 import {enableScreens} from 'react-native-screens';
+import { createStore, combineReducers } from "redux";
+import mealsReducer from './store/reducers/meals';
+import {Provider} from 'react-redux'
 
 enableScreens();
+
+const rootReducer = combineReducers({
+    meals: mealsReducer
+});
+const store = createStore(rootReducer);
 
 const fetchFonts = () => {
     return Font.loadAsync(
@@ -27,7 +35,8 @@ export default function App() {
     }
 
     return (
-        <MealsNavigator />
+        <Provide store={store}>
+        <MealsNavigator /></Provide>
 
     );
 }
